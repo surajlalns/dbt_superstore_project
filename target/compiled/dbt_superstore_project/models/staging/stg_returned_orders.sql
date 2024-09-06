034f4bb82bbccde91e14f8ecf432e2027f27837c
surajlalns <suraj.lal.ns@gmail.com>
2024-09-06 17:11:28 -0300

    Delete target directory

--- target/compiled/dbt_superstore_project/models/staging/stg_returned_orders.sql
+++
@@ -1,5 +0,0 @@
--- models\staging\stg_returned_orders.sql
-select
-     distinct order_id
-
-from dbtsurajraw.superstoresuraj.returned_orders
--- target/compiled/dbt_superstore_project/models/staging/stg_sales_managers.sql
+++
@@ -1,6 +0,0 @@
--- models\staging\stg_sales_managers.sql
-select
-     person as manager_name
-    ,region
-
-from dbtsurajraw.superstoresuraj.sales_managers
